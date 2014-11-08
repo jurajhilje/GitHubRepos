d23e91cb515e98854088d25780be6044915001c3
Juraj Hilje <jhilje@gmail.com>
2014-11-08 23:25:14 +0100

    Initial Commit

---
+++ GitHubRepos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubRepos
+//
+//  Created by Juraj Hilje on 08/11/14.
+//  Copyright (c) 2014 Juraj Hilje. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
